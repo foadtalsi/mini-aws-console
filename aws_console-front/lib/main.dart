78fc0b99017b258e83646f86c58e1c83382c2754
foadtalsi <foad.talsi@icloud.com>
2026-01-31 12:44:31 +0100

    change the ip for the http request

--- aws_console-front/lib/main.dart
+++ aws_console-front/lib/main.dart
@@ -176,7 +176,7 @@ class _Ec2PageState extends State<Ec2Page> {
       _lastResult = null;
     });
 
-    const url = 'http://44.198.22.236:8000';
+    const url = 'http://44.198.22.236:8000/ec2/launch';
     try {
       final response = await Dio().post(
         url,
